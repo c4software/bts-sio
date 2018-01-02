13c4a952226961b3937ee105270791f2f954fd7d
Valentin Brosseau <vbrosseau@afone.com>
2018-01-02 15:28:14 +0100

    Changement commande

--- build_book.sh
+++ build_book.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-gitbook build && mv _book docs && git add docs/ && git commit -am "Rebuild du GitBook"
+gitbook build && mv _book docs 
+git add docs/ 
+git commit -am "Rebuild du GitBook"
