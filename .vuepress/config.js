47332b1a7032d1fde0a55f942e251d5aa1ee278c
Valentin Brosseau <c4software@gmail.com>
2019-11-11 22:17:15 +0100

    Le menu ne sont plus refermable

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -6,28 +6,29 @@ module.exports = {
   dest: "docs",
   themeConfig: {
     displayAllHeaders: true,
-    sidebarDepth: 1,
+    sidebarDepth: 0,
     sidebar: [
       ["/", "Introduction"],
-      { title: "Aides mémoires", children: [["cheatsheets/git/", "Git"], ["cheatsheets/cordova/", "Cordova"], ["cheatsheets/docker/", "VueJS"], ["cheatsheets/vuejs/", "Docker"]] },
-      { title: "Initiation à Git", children: [["https://cours.brosseau.ovh/cours/sources/revealjs/index.html?source=git", "Slides"], "/tp/git_initiation/"] },
-      { title: "Git en groupe + GitLab", children: [["https://cours.brosseau.ovh/cours/sources/revealjs/index.html?source=gitlab", "Slides"], "/tp/gitlab/"] },
-      { title: "Organisation du code", children: [["https://cours.brosseau.ovh/cours/sources/revealjs/index.html?source=organisations", "Slides"], "/tp/organisation/introduction"] },
-      { title: "Javascript avancé (À venir)", children: [["https://cours.brosseau.ovh/cours/sources/revealjs/index.html?source=javascript_avances", "Slides"], "tp/javascript_avances/introduction"] },
-      { title: "Programmation sécurisée avec OWASP", children: [["https://cours.brosseau.ovh/cours/sources/revealjs/index.html?source=securite_applications", "Slides"], "tp/securite/"] },
-      { title: "Laravel", children: [["https://cours.brosseau.ovh/cours/sources/revealjs/index.html?source=laravel", "Slides"], "/tp/laravel/introduction", "/tp/laravel/application_todo_list", "/tp/laravel/creation_api"] },
-      { title: "Typescript (À venir)", children: [["https://cours.brosseau.ovh/cours/sources/revealjs/index.html?source=typescript", "Slides"]] },
-      { title: "NodeJS  (À venir)", children: [["https://cours.brosseau.ovh/cours/sources/revealjs/index.html?source=nodejs", "Slides"], "/tp/nodejs/api", "/tp/nodejs/firebase"] },
-      { title: "VueJS", children: [["https://cours.brosseau.ovh/cours/sources/revealjs/index.html?source=vuejs", "Slides"], "/tp/vuejs/tp1-vuejs-laravel-api", "/tp/vuejs/tp1", "/tp/vuejs/tp2", "/tp/vuejs/firebase-vuejs"] },
+      { collapsable: false, title: "Aides mémoires", children: [["cheatsheets/git/", "Git"], ["cheatsheets/cordova/", "Cordova"], ["cheatsheets/docker/", "VueJS"], ["cheatsheets/vuejs/", "Docker"]] },
+      { collapsable: false, title: "Initiation à Git", children: [["https://cours.brosseau.ovh/cours/sources/revealjs/index.html?source=git", "Slides"], "/tp/git_initiation/"] },
+      { collapsable: false, title: "Git en groupe + GitLab", children: [["https://cours.brosseau.ovh/cours/sources/revealjs/index.html?source=gitlab", "Slides"], "/tp/gitlab/"] },
+      { collapsable: false, title: "Organisation du code", children: [["https://cours.brosseau.ovh/cours/sources/revealjs/index.html?source=organisations", "Slides"], "/tp/organisation/introduction"] },
+      { collapsable: false, title: "Javascript avancé (À venir)", children: [["https://cours.brosseau.ovh/cours/sources/revealjs/index.html?source=javascript_avances", "Slides"], "tp/javascript_avances/introduction"] },
+      { collapsable: false, title: "Programmation sécurisée avec OWASP", children: [["https://cours.brosseau.ovh/cours/sources/revealjs/index.html?source=securite_applications", "Slides"], "tp/securite/"] },
+      { collapsable: false, title: "Laravel", children: [["https://cours.brosseau.ovh/cours/sources/revealjs/index.html?source=laravel", "Slides"], "/tp/laravel/introduction", "/tp/laravel/application_todo_list", "/tp/laravel/creation_api"] },
+      { collapsable: false, title: "Typescript (À venir)", children: [["https://cours.brosseau.ovh/cours/sources/revealjs/index.html?source=typescript", "Slides"]] },
+      { collapsable: false, title: "NodeJS  (À venir)", children: [["https://cours.brosseau.ovh/cours/sources/revealjs/index.html?source=nodejs", "Slides"], "/tp/nodejs/api", "/tp/nodejs/firebase"] },
+      { collapsable: false, title: "VueJS", children: [["https://cours.brosseau.ovh/cours/sources/revealjs/index.html?source=vuejs", "Slides"], "/tp/vuejs/tp1-vuejs-laravel-api", "/tp/vuejs/tp1", "/tp/vuejs/tp2", "/tp/vuejs/firebase-vuejs"] },
       {
+        collapsable: false,
         title: "VueJS exemple",
         children: [["https://cours.brosseau.ovh/demo/vuejs/demo1/index.html", "Demo 1"], ["https://cours.brosseau.ovh/demo/vuejs/counter/index.html", "Compteur 1"], ["https://cours.brosseau.ovh/demo/vuejs/clock/index.html", "Horloge"], ["https://cours.brosseau.ovh/demo/vuejs/timestamp-color/index.html", "Horloge 2"], ["https://cours.brosseau.ovh/demo/vuejs/sound/index.html", "Sonomètre"]]
       },
-      { title: "Cordova + VueJS", children: [["https://cours.brosseau.ovh/cours/sources/revealjs/index.html?source=cordova", "Slides"], "/tp/cordova/decouverte", "/tp/cordova/vuejs_cordova", "/tp/api/doc_api_led"] },
-      { title: "Docker", children: [["https://rawgit.com/c4software/bts/master/cours/docker/", "Slides"], "/tp/docker/introduction", "/tp/docker/dockerfile", "/tp/docker/docker_compose"] },
-      { title: "GitLab-CI", children: [["https://cours.brosseau.ovh/cours/sources/revealjs/index.html?source=gitlabci", "Slides"], "/tp/ci/pages", "/tp/ci/ci-hybride"] },
-      { title: "Python : Framework Flask", children: [["https://cours.brosseau.ovh/cours/sources/revealjs/index.html?source=python", "Slides"], "/tp/python/flask", "/tp/python/flask_todolist_api"] },
-      { title: "Android", children: [["https://cours.brosseau.ovh/cours/sources/revealjs/index.html?source=android", "Slides"]] }
+      { collapsable: false, title: "Cordova + VueJS", children: [["https://cours.brosseau.ovh/cours/sources/revealjs/index.html?source=cordova", "Slides"], "/tp/cordova/decouverte", "/tp/cordova/vuejs_cordova", "/tp/api/doc_api_led"] },
+      { collapsable: false, title: "Docker", children: [["https://rawgit.com/c4software/bts/master/cours/docker/", "Slides"], "/tp/docker/introduction", "/tp/docker/dockerfile", "/tp/docker/docker_compose"] },
+      { collapsable: false, title: "GitLab-CI", children: [["https://cours.brosseau.ovh/cours/sources/revealjs/index.html?source=gitlabci", "Slides"], "/tp/ci/pages", "/tp/ci/ci-hybride"] },
+      { collapsable: false, title: "Python : Framework Flask", children: [["https://cours.brosseau.ovh/cours/sources/revealjs/index.html?source=python", "Slides"], "/tp/python/flask", "/tp/python/flask_todolist_api"] },
+      { collapsable: false, title: "Android", children: [["https://cours.brosseau.ovh/cours/sources/revealjs/index.html?source=android", "Slides"]] }
     ]
   }
 };
