b01a8d5a5181e4a8f254087086676916507e2e56
Valentin Brosseau <c4software@gmail.com>
2019-12-10 14:15:26 +0100

    Split et simplification du code

--- .vuepress/public/demo/vuejs/1-2-Vue/src/views/shots.js
+++ .vuepress/public/demo/vuejs/1-2-Vue/src/views/shots.js
@@ -1,6 +1,38 @@
 import {stunConfig} from "../config/index.js";
 
 export default {
+  // language=Vue
+  template: `
+      <div class="pad50">
+          <script src="https://webrtc.github.io/adapter/adapter-latest.js"/>
+
+          <div class="grid">
+              <div>
+                  <h1>Offer</h1>
+                  <div>
+                      <textarea readonly @keyup.enter="createMyOffer" v-html="myOffer"/>
+                      <button @click="createMyOffer">Create my offer</button>
+                  </div>
+                  <div>
+                      <textarea @keyup.enter="setRemoteOffer" type='text' v-model="remoteOffer"/>
+                      <button @click="setRemoteOffer">Set remote Offer</button>
+                  </div>
+              </div>
+              
+              <div>
+                  <h1>Answer</h1>
+                  <textarea type='text' readonly v-model="answer"/>
+              </div>
+          </div>
+
+          <div v-if="connected">
+              <h1>Chat !</h1>
+              <div>
+                  <p v-for="m in messages">{{m}}</p>
+              </div>
+              <input type="text" v-model="myMessage" @keyup.enter="triggerSendMessage">
+          </div>
+      </div>`,
   name: "shots",
   data() {
     return {
@@ -10,64 +42,60 @@ export default {
       remoteOffer: "",
       answer: "",
 
+      connected: false,
       messages: [],
       myMessage: "",
     }
   },
   mounted() {
     this.pc = new RTCPeerConnection(stunConfig);
-    this.channel = this.pc.createDataChannel("chat");
-    this.channel.onopen = (e) => this.messages.push("Connected");
-    this.channel.onclose = (e) => this.messages.push("Disconnected");
-    this.channel.onerror = (e) => this.messages.push("An error occured");
-    this.channel.onmessage = (e) => this.messages.push(`Remote: ${e.data}`);
+    this.initChannel();
   },
   methods: {
-    createMyOffer() {
+    initChannel() {
+      this.channel = this.pc.createDataChannel("chat");
+      this.channel.onopen = (e) => {
+        this.connected = true;
+        this.messages.push("Connected");
+      };
 
+      this.channel.onclose = (e) => {
+        this.connected = false;
+        this.messages.push("Disconnected");
+      };
+
+      this.channel.onerror = (e) => this.messages.push("An error occured");
+      this.channel.onmessage = (e) => this.messages.push(`Remote: ${e.data}`);
+    },
+    createMyOffer() {
       this.pc.createOffer()
         .then((offer) => {
-          this.myOffer = offer.sdp;
+          this.myOffer = JSON.stringify(offer);
           return this.pc.setLocalDescription(offer);
         });
     },
     setRemoteOffer() {
-      const desc = new RTCSessionDescription({type: "offer", sdp: this.remoteOffer});
+      let remoteOfferParsed = JSON.parse(this.remoteOffer);
+      const desc = new RTCSessionDescription({type: "offer", sdp: remoteOfferParsed.sdp});
       this.pc
         .setRemoteDescription(desc)
         .then(() => this.pc.createAnswer())
         .then(d => this.pc.setLocalDescription(d))
         .then(value => {
-          this.answer = this.pc.localDescription.sdp;
+          this.answer = JSON.stringify(this.pc.localDescription);
+        })
+        .catch(err => {
+          alert(err);
         })
     },
     triggerSendMessage() {
-      this.messages.push(`You: ${this.myMessage}`);
-      this.channel.send(this.myMessage);
-      this.myMessage = "";
+      try {
+        this.channel.send(this.myMessage);
+        this.messages.push(`You: ${this.myMessage}`);
+        this.myMessage = "";
+      } catch (e) {
+
+      }
     }
-  },
-  // language=Vue
-  template: `
-      <div class="pad50">
-          <script src="https://webrtc.github.io/adapter/adapter-latest.js"/>
-          <h1>Offer</h1>
-          <div>
-              <textarea readonly @keyup.enter="createMyOffer" v-html="myOffer"/>
-              <button @click="createMyOffer">Create my offer</button>
-          </div>
-          <div>
-              <textarea @keyup.enter="setRemoteOffer" type='text' v-model="remoteOffer"/>
-              <button @click="setRemoteOffer">Set remote Offer</button>
-          </div>
-
-          <h1>Answer</h1>
-          <textarea type='text' v-model="answer"/>
-
-          <h1>Chat !</h1>
-          <div>
-              <p v-for="m in messages">{{m}}</p>
-          </div>
-          <input type="text" v-model="myMessage" @keyup.enter="triggerSendMessage">
-      </div>`
+  }
 };
