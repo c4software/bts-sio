b20e82f30f2d6b5e911b40fcba453ccd333f9d8c
Valentin Brosseau <c4software@gmail.com>
2020-01-18 11:03:32 +0100

    Update Tap O Tap

--- .vuepress/public/demo/vuejs/1-2-Vue/src/views/tapotap.js
+++ .vuepress/public/demo/vuejs/1-2-Vue/src/views/tapotap.js
@@ -5,6 +5,26 @@ export default {
         return 100 - this.red;
     }
   },
+  mounted(){
+    window.addEventListener('keyup', this.keyHandler, true);
+  },
+  beforeDestroy(){
+    window.removeEventListener('keyup', this.keyHandler, true);
+  },
+  methods: {
+    keyHandler(e){
+      if(this.red <= 0 ){
+        this.red = 50;
+        return;
+      }
+
+      if(e.keyCode == 37 || e.keyCode == 38){
+        this.red += 10
+      } else if(e.keyCode == 39 || e.keyCode == 40) {
+        this.red -= 10
+      }
+    }
+  },
   data() {
     return {
         red: 50,
@@ -12,7 +32,7 @@ export default {
   },
   template: 
   `<div class='tapotap'>
-    <div class='red' :style='{height: red + "vh"}' @click='red+=10'></div>
+    <div class='red' :style='{height: red + "vh"}' @keyup.left @click='red+=10'></div>
     <div class='green' :style='{height: green + "vh"}' @click='red-=10'></div>
     <div class="winner" v-if="red >= 100 || green >= 100">
         <div style="width: 100%">
