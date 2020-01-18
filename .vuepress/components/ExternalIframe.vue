f2e3cb4395b6adfcb3be737a01abc67f37c82a4a
Valentin Brosseau <c4software@gmail.com>
2020-01-18 10:43:38 +0100

    Fond par défaut

--- .vuepress/components/ExternalIframe.vue
+++ .vuepress/components/ExternalIframe.vue
@@ -30,6 +30,7 @@ export default {
     iframe{
         width: 100%;
         height: 50vh;
+        background: white;
     }
 
     button{
--- .vuepress/components/Sample.vue
+++ .vuepress/components/Sample.vue
@@ -20,9 +20,3 @@ export default {
   }
 };
 </script>
-
-<style scoped>
-    iframe {
-        background: white;
-    }
-</style>
