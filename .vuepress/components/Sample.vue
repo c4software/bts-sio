080827674940da238c7efa0ad6d7c742379becb2
Valentin Brosseau <c4software@gmail.com>
2020-01-18 10:32:45 +0100

    Migration vers le composant Sample

---
+++ .vuepress/components/Sample.vue
@@ -0,0 +1,28 @@
+<template>
+  <ExternalIframe :src="samplePath"></ExternalIframe>
+</template>
+
+<script>
+import externalIframe from "./ExternalIframe.vue";
+
+export default {
+  name: "Sample",
+  components: { externalIframe },
+  props:{
+      src: {
+          required: true
+      }
+  },
+  computed: {
+      samplePath(){
+          return `/demo/${this.src}/index.html`;
+      }
+  }
+};
+</script>
+
+<style scoped>
+    iframe {
+        background: white;
+    }
+</style>
