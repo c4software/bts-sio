802d65890d05fa663d4a82f8864d461291198381
Valentin Brosseau <c4software@gmail.com>
2020-07-09 08:50:33 +0200

    Ajout meta pour corriger problème Firefox

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -17,7 +17,8 @@ module.exports = {
   lang: "fr-FR",
   dest: "docs",
   head: [
-    ['link', { rel: 'icon', href: '/icons/icon-72x72.png' }],
+		['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1.0' }],
+		['link', { rel: 'icon', href: '/icons/icon-72x72.png' }],
     ['link', { rel: 'manifest', href: '/manifest.json' }],
     ['meta', { name: 'theme-color', content: '#3e80af' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
