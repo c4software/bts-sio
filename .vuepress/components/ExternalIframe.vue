25650be58e702d19d8527fe4a642686dbcfd5714
Valentin Brosseau <c4software@gmail.com>
2020-09-11 22:51:39 +0200

    Bouton fullscreen

--- .vuepress/components/ExternalIframe.vue
+++ .vuepress/components/ExternalIframe.vue
@@ -1,47 +1,53 @@
 <template>
   <div>
-    <iframe :src="this.src" frameborder="0" ref='iframe' />
-    <button @click="requestFullscreen">Afficher en plein écran</button>
+    <iframe :src="this.src" frameborder="0" ref="iframe" />
+    <button @click="requestFullscreen">
+      <img src="/fullscreen.svg" />
+    </button>
   </div>
 </template>
 
 <script>
 export default {
-    name: "ExternalIframe",
-    props: {
-        src: {
-            required: true
-        }
+  name: "ExternalIframe",
+  props: {
+    src: {
+      required: true,
     },
-    methods: {
-        requestFullscreen(){
-            this.$refs['iframe'].requestFullscreen();
-            this.$refs['iframe'].focus();
+  },
+  methods: {
+    requestFullscreen() {
+      this.$refs["iframe"].requestFullscreen();
+      this.$refs["iframe"].focus();
 
-            try{
-                ga('send', 'event', 'requestFullscreen', this.src);
-            } catch(err){}
-        }
-    }
-}
+      try {
+        ga("send", "event", "requestFullscreen", this.src);
+      } catch (err) {}
+    },
+  },
+};
 </script>
 
 <style scoped>
-    iframe{
-        width: 100%;
-        height: 50vh;
-        background: white;
-    }
+iframe {
+  width: 100%;
+  height: 50vh;
+  background: white;
+}
 
-    button{
-        display: block;
-        margin: 20px auto;
-        font-size: 1.1em;
-        border: none;
-        border-radius: 5px;
-        padding: 5px;
-        background-color: #d5daea;
-        cursor: pointer;
-        user-select: none;
-    }
+button {
+  padding: 5px;
+  background-color: #22232387;
+  color: white;
+  cursor: pointer;
+  user-select: none;
+
+  display: block;
+  font-size: 1.1em;
+  border: none;
+  border-radius: 5px;
+  position: relative;
+  top: -52px;
+  right: -20px;
+}
 </style>
