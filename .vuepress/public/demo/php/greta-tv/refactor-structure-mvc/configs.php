c49db98e475eb2ecb548eee2f0a55720f1d353f9
Valentin Brosseau <c4software@gmail.com>
2021-08-31 14:37:02 +0200

    Update DSN

--- .vuepress/public/demo/php/greta-tv/refactor-structure-mvc/configs.php
+++ .vuepress/public/demo/php/greta-tv/refactor-structure-mvc/configs.php
@@ -9,7 +9,7 @@ $DEBUG = getenv("MVC_DEBUG") ?: true;
 return array(
     "DB_USER" => $DB_USER,
     "DB_PASSWORD" => $DB_PASSWORD,
-    "DB_DSN" => "mysql:host=$DB_SERVER;dbname=$DB_DATABASE",
+    "DB_DSN" => "mysql:host=$DB_SERVER;dbname=$DB_DATABASE;charset=utf8",
     "DEBUG" => $DEBUG
 );
 
