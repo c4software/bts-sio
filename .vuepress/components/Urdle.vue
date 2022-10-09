4de4b8398d45b85a8e73923a90f53172a1c7994c
Valentin Brosseau <c4software@gmail.com>
2022-10-09 12:59:58 +0200

    Structure Urdle

---
+++ .vuepress/components/Urdle.vue
@@ -0,0 +1,158 @@
+<template>
+    <div>
+        <div class="container">
+            <div class="col center">
+                <select v-model="selected" class="form-select">
+                    <option value="-1">Consulter les exercices</option>
+                    <option v-for="level in levels" :value="level">Jour {{level}}</option>
+                </select>
+            </div>
+        </div>
+
+
+
+        <div class="container">
+            <div class="col center">
+                <input v-if="canNext" @click="selected++" type="button" value="Suivant" class="btn btn-primary" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "Urdle",
+  watch: {
+    selected(){
+        localStorage.setItem('urdle', this.selected);
+    }
+  },
+  computed: {
+    canNext(){
+        return this.selected <= this.levels.length - 1;
+    }
+  },
+  data() {
+    return {
+        selected: localStorage.getItem('urdle') || -1,
+        levels: [...Array(10).keys()].map(it => it+1)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.hidden {
+  display: none;
+}
+
+.container {
+  display: flex;
+  width: 100%;
+}
+
+.col {
+  flex-grow: 1;
+  padding: 20px;
+}
+
+.col.center {
+  text-align: center;
+}
+
+.form-control {
+  display: block;
+  width: 100%;
+  height: calc(1.5em + 0.75rem + 2px);
+  padding: 0.375rem 0.75rem;
+  font-size: 1rem;
+  font-weight: 400;
+  line-height: 1.5;
+  color: #495057;
+  background-color: #fff;
+  background-clip: padding-box;
+  border: 1px solid #ced4da;
+  border-radius: 0.25rem;
+  transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+}
+
+.form-control:focus {
+  color: #495057;
+  background-color: #fff;
+  border-color: #80bdff;
+  outline: 0;
+  box-shadow: 0 0 0 0.2rem rgba(0, 123, 255, 0.25);
+}
+
+textarea.form-control {
+  height: auto;
+}
+
+.btn {
+  display: inline-block;
+  font-weight: 400;
+  color: #212529;
+  text-align: center;
+  vertical-align: middle;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  background-color: transparent;
+  border: 1px solid transparent;
+  padding: 0.375rem 0.75rem;
+  font-size: 1rem;
+  line-height: 1.5;
+  border-radius: 0.25rem;
+  transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out,
+    border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+}
+
+.form-select {
+    display: block;
+    width: 100%;
+    padding: 0.375rem 2.25rem 0.375rem 0.75rem;
+    -moz-padding-start: calc(0.75rem - 3px);
+    font-size: 1rem;
+    font-weight: 400;
+    line-height: 1.5;
+    color: #212529;
+    background-color: #fff;
+    background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16'%3e%3cpath fill='none' stroke='%23343a40' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M2 5l6 6 6-6'/%3e%3c/svg%3e");
+    background-repeat: no-repeat;
+    background-position: right 0.75rem center;
+    background-size: 16px 12px;
+    border: 1px solid #ced4da;
+    border-radius: 0.25rem;
+    transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+}
+
+select {
+    word-wrap: normal;
+}
+button, select {
+    text-transform: none;
+}
+
+button, input, optgroup, select, textarea {
+    margin: 0;
+    font-family: inherit;
+    font-size: inherit;
+    line-height: inherit;
+}
+
+.btn-primary {
+  color: #fff;
+  background-color: #007bff;
+  border-color: #007bff;
+}
+
+@media only screen and (max-width: 960px) {
+  .container {
+    display: block;
+  }
+}
+</style>
---
+++ pages/urdle.md
@@ -0,0 +1,13 @@
+---
+description: Exercice d'entrainement lecture d'UML.
+---
+
+# Exercices d'entraînement à la lecture d'UML
+
+Pour progresser, il faut pratiquer, vous trouverez ici des exercices permettant de vous entraîner à la lecture d'UML. La lecture d'UML est un élément important pour comprendre les études de cas, celles-ci étant à réaliser sur papier, je vous conseille vivement de travailler les exercices sur feuilles.
+
+Cette page a été conçue dans le but de réaliser un exercice par jour.
+
+Je suis à disposition pour les corrections via Slack ou par [email](./contact.md).
+
+<Urdle />
