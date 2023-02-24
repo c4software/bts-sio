771399208843ac1a848f89d1913f8563cd4e0180
Valentin Brosseau <c4software@gmail.com>
2023-02-24 15:57:54 +0100

    CI

--- .vuepress/components/overridingTheme/Page.vue
+++ .vuepress/components/overridingTheme/Page.vue
@@ -11,23 +11,25 @@ const navBarHeight = ref(0);
 const { y } = useWindowScroll();
 
 nextTick(() => {
-    if(document){
-        // Récupération des titres de la page (h2)
-        document.querySelectorAll('.content h2').forEach((h2: any) => {
-            onThisPage.value.push({
-                el: h2,
-                id: h2.id,
-                text: h2.innerText.replace("#", "")
-            })
-        })
-
-        // Vérification de l'existence d'une table des matières dans la page
-        // (si oui, on affiche la colonne de droite)
-        hasDocumentToc.value = document.getElementsByClassName("table-of-contents").length > 0;
-
-        // Récupération de la hauteur de la barre de navigation (pour gérer la classe "active" sur le bon élément)
-        navBarHeight.value = document.getElementsByClassName("navbar")[0]?.clientHeight ?? 0
+    if (typeof window === 'undefined' || !window.document) {
+        return
     }
+    
+    // Récupération des titres de la page (h2)
+    document.querySelectorAll('.content h2').forEach((h2: any) => {
+        onThisPage.value.push({
+            el: h2,
+            id: h2.id,
+            text: h2.innerText.replace("#", "")
+        })
+    })
+
+    // Vérification de l'existence d'une table des matières dans la page
+    // (si oui, on affiche la colonne de droite)
+    hasDocumentToc.value = document.getElementsByClassName("table-of-contents").length > 0;
+
+    // Récupération de la hauteur de la barre de navigation (pour gérer la classe "active" sur le bon élément)
+    navBarHeight.value = document.getElementsByClassName("navbar")[0]?.clientHeight ?? 0
 })
 
 // Computed, Est-ce qu'on doit afficher la colonne de droite ?
